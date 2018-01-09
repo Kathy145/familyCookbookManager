25d8f511b8f310a79be516738127ab0e02cc605d
Kathy Lambert <Kathy.Lambert@LotusDesignsAgency.com>
2018-01-08 22:58:14 -0500

    Updated connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,2 +1,16 @@
-const mongoose = require
+const mongoose = require('./models/recipe')
 
+mongoose.Promise = Promise;
+
+const mongoUri = 'mongodb://localhost/recipemanager';
+
+mongoose
+    .connect(mongoUri, { useMongoClient: true })
+    .then(connection =>
+        console.log(
+            `Connection established to db '${connection.db.databaseName}'`
+        )
+    )
+    .catch(connectionError =>
+        console.log('Connection failed!', connectionError)
+    );
--- db/models/recipe.js
+++ db/models/recipe.js
@@ -1,4 +1,4 @@
-const mongoose = require('cookbook)
+const mongoose = require('cookbook')
 
 const RecipeSchema = new mongoose.Schema({
   title: String,
