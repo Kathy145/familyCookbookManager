39f51e8287b36883f31e05e9ff826edfef9830ee
Kathy145 <Kathy.Lambert@LotusDesignsAgency.com>
2018-02-18 21:53:00 -0500

    Added replaced public folder

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('recipes-index', {});
+    res.render('app-welcome', {});
 });
 
 
