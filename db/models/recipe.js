9fe2d2fc3a1c232d393b47012efc63504d5753a0
Kathy145 <Kathy.Lambert@LotusDesignsAgency.com>
2018-03-04 14:54:23 -0500

    Added imageUrl with accompanying picture

--- db/models/recipe.js
+++ db/models/recipe.js
@@ -5,6 +5,7 @@ const RecipeSchema = new mongoose.Schema({
   description: String,
   ingredients: String,
   instructions: String,
+  imageUrl: String,
   cookbook: String,
   origindate: String
 })
